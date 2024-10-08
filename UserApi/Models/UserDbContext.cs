e79f15f472841f15a2f428ae555048cd00cd7243
BolgarMilan <bolgarm@kkszki.hu>
2024-10-08 12:18:54 +0200

    SQL export added

---
+++ UserApi/Models/UserDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace UserApi.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext()
+        {
+
+        }
+        
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Users; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+
+        DbSet<User> Newuser { get; set; } = null!;
+    }
+}
